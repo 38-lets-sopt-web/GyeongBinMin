b498e277b9e22fc163c30a76ea600a413cfbccff
gyeongbibin <151108886+gyeongbibin@users.noreply.github.com>
2026-04-22 22:21:32 +0900

    chore : 결제수단 & 카테고리 상수 추가

--- Week2/Homework/js/constants.js
+++ Week2/Homework/js/constants.js
@@ -1,2 +1,18 @@
 export const STORAGE_KEY = "expenseData";
 
+export const CATEGORY_LABEL = {
+  food: "식비",
+  salary: "월급",
+  culture: "문화",
+  transport: "교통",
+  living: "생활",
+  other: "기타",
+};
+
+export const PAYMENT_LABEL = {
+  cash: "현금",
+  "credit-card": "신용카드",
+  "debit-card": "체크카드",
+  other: "기타",
+};
+
